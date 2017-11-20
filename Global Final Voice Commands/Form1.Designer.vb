a2f5c69cf958a2bf039ddcf8a1d4c13a7c48565c
PlGGS <bbor0422@gmail.com>
2017-11-19 20:05:05 -0600

    Added files via upload

---
+++ Global Final Voice Commands/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFinal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.timVolMax = New System.Windows.Forms.Timer(Me.components)
+        Me.timEndCounter = New System.Windows.Forms.Timer(Me.components)
+        Me.timVolDown = New System.Windows.Forms.Timer(Me.components)
+        Me.timVolExecute = New System.Windows.Forms.Timer(Me.components)
+        Me.timVolUp = New System.Windows.Forms.Timer(Me.components)
+        Me.timSelfDestruct = New System.Windows.Forms.Timer(Me.components)
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'timVolMax
+        '
+        '
+        'timEndCounter
+        '
+        Me.timEndCounter.Interval = 1000
+        '
+        'timVolDown
+        '
+        '
+        'timVolExecute
+        '
+        Me.timVolExecute.Interval = 15
+        '
+        'timVolUp
+        '
+        '
+        'timSelfDestruct
+        '
+        Me.timSelfDestruct.Interval = 1000
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(59, 44)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(183, 82)
+        Me.btnTest.TabIndex = 0
+        Me.btnTest.Text = "Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'frmFinal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 431)
+        Me.Controls.Add(Me.btnTest)
+        Me.DoubleBuffered = True
+        Me.Location = New System.Drawing.Point(900, 5)
+        Me.Name = "frmFinal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents timVolMax As Timer
+    Friend WithEvents timEndCounter As Timer
+    Friend WithEvents timVolDown As Timer
+    Friend WithEvents timVolExecute As Timer
+    Friend WithEvents timVolUp As Timer
+    Friend WithEvents timSelfDestruct As Timer
+    Friend WithEvents btnTest As Button
+End Class
---
+++ Global Final Voice Commands/Form1.vb
@@ -0,0 +1,197 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Speech
+
+Public Class frmFinal
+
+    Private Const APPCOMMAND_VOLUME_MUTE As Integer = &H80000
+    Private Const APPCOMMAND_VOLUME_UP As Integer = &HA0000
+    Private Const APPCOMMAND_VOLUME_DOWN As Integer = &H90000
+    Private Const WM_APPCOMMAND As Integer = &H319
+
+    Dim WithEvents reco As New Recognition.SpeechRecognitionEngine
+
+    Dim destructCounter As Single = -1
+
+    Dim endCounter As Single
+
+    Dim volTime As Single
+
+    Dim vol As Single
+
+    Dim Muted As Boolean = False
+
+    <DllImport("user32.dll")>
+    Public Shared Function SendMessageW(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Me.Hide()
+
+        reco.SetInputToDefaultAudioDevice()
+
+        Dim gram As New Recognition.SrgsGrammar.SrgsDocument
+
+        Dim grammerRule As New Recognition.SrgsGrammar.SrgsRule("command")
+
+        Dim commandList As New Recognition.SrgsGrammar.SrgsOneOf("hacking", "yes", "computer, end presentation")
+
+        grammerRule.Add(commandList)
+
+        gram.Rules.Add(grammerRule)
+
+        gram.Root = grammerRule
+
+        reco.LoadGrammar(New Recognition.Grammar(gram))
+
+        reco.RecognizeAsync()
+
+        endCounter = -1
+
+        volTime = -1
+
+        vol = 0
+
+    End Sub
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles reco.RecognizeCompleted
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub timSelfDestruct_Tick(sender As Object, e As EventArgs) Handles timSelfDestruct.Tick
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        destructCounter += 1
+
+        If destructCounter = 6 Then
+
+            timEndCounter.Stop()
+
+            destructCounter = -1
+
+        End If
+
+        If destructCounter = -1 Then
+
+            timSelfDestruct.Stop()
+
+            synth.SpeakAsync("Blake, that was terrible acting, my scans show that nobody thought I was actually going to explode.")
+
+        ElseIf destructCounter = 1 Then
+
+            synth.SpeakAsync("five")
+
+        ElseIf destructCounter = 2 Then
+
+            synth.SpeakAsync("four")
+
+        ElseIf destructCounter = 3 Then
+
+            synth.SpeakAsync("three")
+
+        ElseIf destructCounter = 4 Then
+
+            synth.SpeakAsync("two")
+
+        ElseIf destructCounter = 5 Then
+
+            synth.SpeakAsync("one")
+
+        End If
+
+    End Sub
+
+    Private Sub timEndCounter_Tick(sender As Object, e As EventArgs) Handles timEndCounter.Tick
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        endCounter += 1
+
+        If endCounter = 6 Then
+
+            timEndCounter.Stop()
+
+            endCounter = -1
+
+        End If
+
+        If endCounter = -1 Then
+
+            synth.SpeakAsync("Okay, nevermind")
+
+        ElseIf endCounter = 1 Then
+
+            synth.SpeakAsync("five")
+
+        ElseIf endCounter = 2 Then
+
+            synth.SpeakAsync("four")
+
+        ElseIf endCounter = 3 Then
+
+            synth.SpeakAsync("three")
+
+        ElseIf endCounter = 4 Then
+
+            synth.SpeakAsync("two")
+
+        ElseIf endCounter = 5 Then
+
+            synth.SpeakAsync("one")
+
+        End If
+
+    End Sub
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles reco.SpeechRecognized
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        Dim chrome() As Process
+
+        chrome = Process.GetProcessesByName("chrome")
+
+        If e.Result.Text = "yes" And timEndCounter.Enabled = True And endCounter <= 5 And endCounter <> -1 Then
+
+            timEndCounter.Stop()
+
+            For i As Integer = 0 To chrome.Count - 1
+
+                chrome(i).CloseMainWindow()
+
+            Next i
+
+            Application.Exit()
+
+        End If
+
+        Select Case e.Result.Text
+
+            Case "hacking"
+
+                synth.SpeakAsync("self destruct initiated")
+
+                timSelfDestruct.Start()
+
+            Case "computer, end presentation"
+
+                synth.SpeakAsync("are you sure?")
+
+                timEndCounter.Start()
+
+        End Select
+
+    End Sub
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+
+        Dim synth As New Synthesis.SpeechSynthesizer
+
+        synth.SpeakAsync("Testing")
+
+    End Sub
+End Class
